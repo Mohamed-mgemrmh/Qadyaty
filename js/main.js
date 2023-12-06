3d8c48fcb4aeb58b7af430a315a0cb68172db831
Mohamed-mgemrmh <125925159+Mohamed-mgemrmh@users.noreply.github.com>
2023-12-07 01:44:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-// landing page gsap animation;
-gsap.from(".header", { duration: 1, y: "-100%", ease: "slow" });
-gsap.from(".animate", { duration: 1, opacity: 0, delay: 1, stagger: 0.3 });
-gsap.from(".landing-text", { duration: 1.8, x: "100vw", delay: 1.6 });
-gsap.from(".landing-main", {
-  duration: 1,
-  delay: 3.6,
-  opacity: 0,
-  ease: "bounce",
-});
-
 // auto image slider
 var counter = 1;
 setInterval(() => {
@@ -37,13 +26,6 @@ icon.onclick = () => {
   downMenu.classList.toggle("down-menu-small-clicked");
 };
 
-icon.onclick = () => {
-  firstXClass.classList.toggle("first-x-class");
-  secondXClass.classList.toggle("second-x-class");
-  thirdXClass.classList.toggle("third-x-class");
-  downMenu.classList.toggle("down-menu-clicked");
-  downMenu.classList.toggle("down-menu-small-clicked");
-};
 link.forEach((ele) => {
   remove(ele);
 });
